5070c63daf1d06325342535fa29c96d5c644aa5e
Tarun Kamboj <tarunkamboj711@gmail.com>
2021-10-02 12:14:32 +0530

    #2 shown details of friends and their payment info.

--- index.cpp
+++ index.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
- 
-int main ()
-{
-    cout<<"Hello World";
-     
-    return 0;
+
+int main() {
+	int noOfFriends;
+	cin >> noOfFriends;
+	
+	string friend[noOfFriends], moneyPaid[noOfFriends];
+	cout << "Enter friend with payment details";
+	for(int i=0; i<noOfFriends; i++){
+		cin >> friend[i] >> moneyPaid[i];
+	}
+	
+	for(int i=0; i<noOfFriends; i++){
+		cout << "Freind " << friend[i] << " paids " << moneyPaid[i] <<" amount.\n";
+	}
+	
+	return 0;
 }
